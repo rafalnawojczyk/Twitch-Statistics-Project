585b5ade50f56005e0e58fe5b47bda86d269fc88
rafalnawojczyk <rafalnawojczyk@gmail.com>
2022-11-10 11:32:08 +0100

    Test config

--- components/StatsByMonth/StatsLabel.test.tsx
+++ components/StatsByMonth/StatsLabel.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import StatsLabel from "./StatsLabel";
-import '@testing-library/jest-dom'
+import "@testing-library/jest-dom";
 
 // check if it renders title span
 // check if upperTitle class login works as intended
